990921ed7c67e80f5056f030996d32a28a5aa632
Irakliy <irakliy81@yahoo.com>
2017-01-06 18:59:58 -0800

    minor update to lua script

--- bin/index.js
+++ bin/index.js
@@ -60,12 +60,18 @@ const script = `
 	
 	if redis.call("EXISTS", KEYS[1]) == 1 then
 		redis.call("ZREMRANGEBYSCORE", KEYS[1], "-inf", timestamp - window * 1000)
-		if redis.call("ZCOUNT", KEYS[1], "-inf", "+inf") >= limit then
+		local count = redis.call("ZCARD", KEYS[1])
+		if count >= limit then
+			if count < limit * 2 then
+				redis.call("ZADD", KEYS[1], timestamp, timestamp)
+				redis.call("EXPIRE", KEYS[1], window)
+			end
+
 			local first_timestamp = tonumber(redis.call("ZRANGE", KEYS[1], 0, 0)[1])
 			return window - math.ceil((timestamp - first_timestamp) / 1000)
 		end
 	end
-	
+
 	redis.call("ZADD", KEYS[1], timestamp, timestamp)
 	redis.call("EXPIRE", KEYS[1], window)
 	return 0
